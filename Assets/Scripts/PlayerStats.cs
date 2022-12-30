6bc2099f56c04bf3b1c51e158ae819f873333256
SneakFox96 <115126119+SneakFox96@users.noreply.github.com>
2022-12-30 14:21:28 -0500

    Update PlayerStats.cs
    added a lot of player stats and headers for inspector

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,20 +6,36 @@ using TMPro;
 
 public class PlayerStats : MonoBehaviour
 {
+    [Header("Player Health Attributes")]
+    public Slider healthSlider; //slider for the player health UI
+    public float maxHealth = 100f; //Players max health
+    public float currentHealth; //Players current health
+    public float healthRegen = 0f; //Players health regen
 
-    public TextMeshProUGUI levelUi;
-    public Slider healthSlider;
-    public float maxHealth = 100f;
-    public float maxExp = 100f;
-    public static int MAX_LEVEL = 100;
-    public float currentHealth;
-    public int level;
-    public float fireRate;
-    public float speed;
-    //can scale this later
-    public float currentExp;
+    [Header("Player Level Attributes")]
+    public TextMeshProUGUI levelUi; //ui element for player level
+    public float maxExp = 100f; //player's max exp
+    public static int MAX_LEVEL = 100; //players max level cap
+    public int level; //players current level
+    public float currentExp; //players current exp
+
+    [Header("Player General Attributes")]
+    public float speed; //players current movement speed
+    public float magnetSize; //players exp collider size - magnetism
+    public float playerLuck; //players luck - could be relevant to crits or increased chances of drops
+    public float playerGreed; //players earn more coins based on increase (+ modifer not *)
+
+    [Header("Weapon Attributes")]
+    public float fireRate; //how long until weapon fires again
+    public float weaponDuration; //how long the weapon will last on screen
+    public float weaponArea; //how large the area of the weapon
+    public float weaponProjectileSpeed; //how fast the projectile on weapons will go
+    public float weaponDamage = 1f; //the damage the projectile will do (base weapon dmg * weapon dmg);
+    public float projectileCount; //how many times the weapon will fire at single instance
+    
     Collider enemyCollider;
     Collider expCollider;
+
     // Start is called before the first frame update
     void Start()
     {
