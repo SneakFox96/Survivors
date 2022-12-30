aee450c2d47def7d11640f95dc755e81369e6a0b
SneakFox96 <115126119+SneakFox96@users.noreply.github.com>
2022-12-30 15:29:23 -0500

    Updated Enemy Behaviour
    Ability to change targets

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -19,6 +19,8 @@ public class EnemyBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        target = FindClosestEnemy();
+
         if (!gameObject)
         {
             return;
